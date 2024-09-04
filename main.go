ed278a5790c7f1f61ff215a7f149e1d1e8cb72ca
MwebeCoder27 <mwebraymond13@gmail.com>
2024-09-04 10:05:18 +0300

    init

---
+++ main.go
@@ -0,0 +1,29 @@
+package main
+
+import (
+	"fmt"
+	"strings"
+)
+
+// function to take in a name and return initials
+
+func getInitials(n string) (string, string) {
+	s := strings.ToUpper(n)
+	names := strings.Split(s, " ")
+
+	var initials []string
+	for _, v := range names {
+		initials = append(initials, v[:1])
+	}
+
+	if len(initials) > 1 {
+		return initials[0], initials[1]
+	}
+
+	return initials[0], "_"
+}
+func main() {
+	//test the function
+	fn, sn := getInitials("Mwebe Raymond")
+	fmt.Println(fn, sn)
+}
